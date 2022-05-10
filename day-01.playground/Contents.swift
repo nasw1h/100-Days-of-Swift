b64edb268753ad7d26eaa5e6ad76beb43e3c5a89
naxwh <munnamon5@gmail.com>
2022-05-10 20:26:16 +0530

    day 01

---
+++ day-01.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+//Variables
+var str = "Hello, playground"
+str = "Goodbye"
+
+//Strings and integers
+var age = 38
+var population = 8_000_000
+
+//Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+//Doubles and booleans
+var pi = 3.141
+var awesome = true
+
+//String interpolation
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test results are here: \(str3)"
+
+//Constants
+let taylor = "swift"
+
+//Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
