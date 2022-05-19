f6b50689ea8edf302fdf3fdc497edcb78d7bd3ee
naxwh <munnamon5@gmail.com>
2022-05-19 19:22:43 +0530

    day 08

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+let driving = { (place: String) in
+    print("I'm going to \(place) in my car")
+}
+
+let drivingWithReturn = { (place: String) -> String in
+    return "I'm going to \(place) in my car"
+}
+
+let message = drivingWithReturn("London")
+print(message)
---
+++ day-08.playground/Contents.swift
@@ -0,0 +1,91 @@
+import UIKit
+
+// Creating your own structs
+
+
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+
+//Computed properties
+
+struct Sport1 {
+    var name: String
+    var isOlympicSport: Bool
+
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
+}
+
+let chessBoxing = Sport1(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+//Property observers
+
+struct Progress {
+    var task: String
+    var amount: Int
+}
+
+var progress = Progress(task: "Loading data", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
+struct Progress1 {
+    var task: String
+    var amount: Int {
+        didSet {
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+//Methods
+
+struct City {
+    var population: Int
+
+    func collectTaxes() -> Int {
+        return population * 1000
+    }
+}
+
+let london = City(population: 9_000_000)
+london.collectTaxes()
+
+//Mutating methods
+
+struct Person {
+    var name: String
+
+    mutating func makeAnonymous() {
+        name = "Anonymous"
+    }
+}
+
+var person = Person(name: "Ed")
+person.makeAnonymous()
+
+//Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+
+print(string.count)
+
+print(string.hasPrefix("Do"))
+
+print(string.uppercased())
+
+print(string.sorted())
+
