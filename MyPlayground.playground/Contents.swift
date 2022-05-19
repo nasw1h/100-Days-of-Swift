c13232f512094180cb04a5aea221f64ecfc5832a
naxwh <munnamon5@gmail.com>
2022-05-19 21:02:29 +0530

    day 10

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,2 @@
 import UIKit
 
-let driving = { (place: String) in
-    print("I'm going to \(place) in my car")
-}
-
-let drivingWithReturn = { (place: String) -> String in
-    return "I'm going to \(place) in my car"
-}
-
-let message = drivingWithReturn("London")
-print(message)
