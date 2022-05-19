aa392acbc8fb28d23c7d7b7902141d7b0d820ea0
naxwh <munnamon5@gmail.com>
2022-05-19 19:38:31 +0530

    day 08

--- day-08.playground/Contents.swift
+++ day-08.playground/Contents.swift
@@ -89,3 +89,16 @@ print(string.uppercased())
 
 print(string.sorted())
 
+//Properties and methods of arrays
+
+var toys = ["Woody"]
+
+print(toys.count)
+
+toys.append("Buzz")
+
+toys.firstIndex(of: "Buzz")
+
+print(toys.sorted())
+
+toys.remove(at: 0)
