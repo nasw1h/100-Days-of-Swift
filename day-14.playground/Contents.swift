d6ffb5af38a67b19325f1a602c2d46a7b0093a82
naxwh <munnamon5@gmail.com>
2022-05-25 23:28:27 +0530

    day 14

--- day-14.playground/Contents.swift
+++ day-14.playground/Contents.swift
@@ -1,3 +1,246 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//Functions
+
+func favoriteAlbum() {
+    print("My favorite is Fearless")
+}
+
+favoriteAlbum()
+
+func albumIsTaylor(name: String) -> Bool {
+    if name == "Taylor Swift" { return true }
+    if name == "Fearless" { return true }
+    if name == "Speak Now" { return true }
+    if name == "Red" { return true }
+    if name == "1989" { return true }
+
+    return false
+}
+
+if albumIsTaylor(name: "Red") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+
+if albumIsTaylor(name: "Blue") {
+    print("That's one of hers!")
+} else {
+    print("Who made that?!")
+}
+
+func getMeaningOfLife() -> Int {
+    return 42
+}
+
+func getMeaningOfLife() -> Int {
+    42
+}
+
+//Optionals
+
+func getHaterStatus() -> String {
+    return "Hate"
+}
+
+func getHaterStatus() -> String? {
+    return "Hate"
+}
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+var status: String
+status = getHaterStatus(weather: "rainy")
+
+var status: String?
+status = getHaterStatus(weather: "rainy")
+
+var status = getHaterStatus(weather: "rainy")
+
+func takeHaterAction(status: String) {
+    if status == "Hate" {
+        print("Hating")
+    }
+}
+
+func yearAlbumReleased(name: String) -> Int? {
+    if name == "Taylor Swift" { return 2006 }
+    if name == "Fearless" { return 2008 }
+    if name == "Speak Now" { return 2010 }
+    if name == "Red" { return 2012 }
+    if name == "1989" { return 2014 }
+
+    return nil
+}
+
+var year = yearAlbumReleased(name: "Red")
+
+if year == nil {
+    print("There was an error")
+} else {
+    print("It was released in \(year)")
+}
+
+print("It was released in \(year!)")
+
+//Optional chaining
+
+func albumReleased(year: Int) -> String? {
+    switch year {
+    case 2006: return "Taylor Swift"
+    case 2008: return "Fearless"
+    case 2010: return "Speak Now"
+    case 2012: return "Red"
+    case 2014: return "1989"
+    default: return nil
+    }
+}
+
+let album = albumReleased(year: 2006)
+print("The album is \(album)")
+
+let str = "Hello world"
+print(str.uppercased())
+
+let album = albumReleased(year: 2006)?.uppercased()
+print("The album is \(album)")
+
+let album = albumReleased(year: 2006)?.someOptionalValue?.someOtherOptionalValue?.whatever
+
+let album = albumReleased(year: 2006) ?? "unknown"
+print("The album is \(album)")
+
+//Enumerations
+
+func getHaterStatus(weather: String) -> String? {
+    if weather == "sunny" {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+enum WeatherType {
+    case sun, cloud, rain, wind, snow
+}
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    if weather == WeatherType.sun {
+        return nil
+    } else {
+        return "Hate"
+    }
+}
+
+getHaterStatus(weather: WeatherType.cloud)
+
+func getHaterStatus(weather: WeatherType) -> String? {
+    switch weather {
+    case .sun:
+        return nil
+    case .wind(let speed) where speed < 10:
+        return "meh"
+    case .cloud, .wind:
+        return "dislike"
+    case .rain, .snow:
+        return "hate"
+    }
+}
+
+getHaterStatus(weather: WeatherType.wind(speed: 5))
+
+//Structs
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+print(taylor.clothes)
+print(other.shoes)
+
+struct Person {
+    var clothes: String
+    var shoes: String
+}
+
+let taylor = Person(clothes: "T-shirts", shoes: "sneakers")
+let other = Person(clothes: "short skirts", shoes: "high heels")
+
+var taylorCopy = taylor
+taylorCopy.shoes = "flip flops"
+
+print(taylor)
+print(taylorCopy)
+
+struct Person {
+    var clothes: String
+    var shoes: String
+
+    func describe() {
+        print("I like wearing \(clothes) with \(shoes)")
+    }
+}
+
+//Classes
+
+class Person {
+    var clothes: String
+    var shoes: String
+}
+
+class Person {
+    var clothes: String
+    var shoes: String
+
+    init(clothes: String, shoes: String) {
+        self.clothes = clothes
+        self.shoes = shoes
+    }
+}
+
+class Singer {
+    var name: String
+    var age: Int
+
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+
+    func sing() {
+        print("La la la la")
+    }
+}
+
+var taylor = Singer(name: "Taylor", age: 25)
+taylor.name
+taylor.age
+taylor.sing()
+
+class CountrySinger: Singer {
+
+}
+
+class HeavyMetalSinger: Singer {
+    var noiseLevel: Int
+
+    init(name: String, age: Int, noiseLevel: Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
