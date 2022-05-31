2e0ebd1b474ae014a903bc5707eaf8d8b11448fd
naxwh <munnamon5@gmail.com>
2022-05-31 23:02:13 +0530

    day 17

---
+++ day-17/I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Naswih on 31/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
